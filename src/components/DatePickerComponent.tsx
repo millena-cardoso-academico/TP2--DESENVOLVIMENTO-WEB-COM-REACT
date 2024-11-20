f55597d013bfbdddaf0a2ca49b2591f71399650a
millena-cardoso-academico <millena.santos@al.infnet.edu.br>
2024-11-20 00:28:18 -0300

    god bless this project

--- src/components/DatePickerComponent.tsx
+++ src/components/DatePickerComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 
 const DatePickerComponent: React.FC = () => {
